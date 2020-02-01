6d55472bc8363a6e18fb6229c50123261deb680c
LISA_COOK <lisabpink@gmail.com>
2020-02-01 09:32:42 -0500

    LDC login form, test api, add private route

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
+
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  
+  const [user, setUser] = useState ('');
+  const [password, setPassword] = useState ('');
+
+const useHandler = e => {
+    e.preventDefault();
+    setUser(e.target.value)
+};
+
+const passwordHandler = e => {
+    e.preventDefault();
+setPassword(e.target.value)
+};
+
+const handleSubmit = e => {
+    e.preventDefault();
+  let credentials = {
+    username: user,
+    password: password
+}
+
+axios
+.post('http://localhost:5000/api/login', credentials)
+.then(res => {localStorage.setItem ('token', res.data.payload)
+})
+.catch(err => console.log('error', err.response
+))
+    setUser('');
+    setPassword('')
+}	  
+  
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={user} onChange={useHandler} placeholder="username" />
+        <input type="password" value={password} onChange={passwordHandler} placeholder="password" />
+        <button>Login</button>
+    </form>
     </>
   );
 };
---
+++ client/src/utils/axiosWithAuth.js
@@ -0,0 +1,11 @@
+import axios from './node_modules/axios';
+
+export const axiosWithAuth = () => {
+const token = localStorage.getItem('token');
+return axios.create({
+baseURL: 'http://localhost:5000',
+headers: {
+    Authorization: token
+    }
+});
+}; 
